bfb1e98e08d96406a80b8ef4f738f0012e49b20e
Mackenzie Clark <mackenzie_a_clark@homedepot.com>
2018-02-09 12:04:30 -0800

    Ignore eslint warning about href='#'

--- src/components/link.js
+++ src/components/link.js
@@ -9,6 +9,7 @@ export const Link = ({active, children, onClick}) => {
             e.preventDefault();
             onClick()
         };
+        // eslint-disable-next-line
         return <a href='#' onClick={getOnClick}> {children} </a>;
     }
 };
